33fcd35b5fb735ab1a505aa57bad33c8cbd310c4
foozbat <github@foozbat.net>
2025-11-15 01:15:53 -0600

    Longest Common Prefix

---
+++ 14-longest-common-prefix.cs
@@ -0,0 +1,44 @@
+/*
+    14. Longest Common Prefix
+    https://leetcode.com/problems/longest-common-prefix/
+
+    Write a function to find the longest common prefix string amongst an array of strings.
+
+    If there is no common prefix, return an empty string "".
+
+    
+
+    Example 1:
+
+    Input: strs = ["flower","flow","flight"]
+    Output: "fl"
+
+    Example 2:
+
+    Input: strs = ["dog","racecar","car"]
+    Output: ""
+    Explanation: There is no common prefix among the input strings.
+
+    
+
+    Constraints:
+
+    - 1 <= strs.length <= 200
+    - 0 <= strs[i].length <= 200
+    - strs[i] consists of only lowercase English letters if it is non-empty.
+ */
+
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        string prefix = strs[0];
+
+        for (int i=1; i<strs.Length; i++) {
+            while (!strs[i].StartsWith(prefix))
+                prefix = prefix[..^1];
+            if (prefix.Length == 0)
+                break;
+        }
+
+        return prefix;
+    }
+}
