c31273a26b68dc9cacb6f58d8cd72b5887284356
foozbat <github@foozbat.net>
2025-11-15 22:00:20 -0600

    fix formatting

--- 14-longest-common-prefix.cs
+++ 14-longest-common-prefix.cs
@@ -6,26 +6,19 @@
 
     If there is no common prefix, return an empty string "".
 
-    
-
     Example 1:
-
-    Input: strs = ["flower","flow","flight"]
-    Output: "fl"
+        Input: strs = ["flower","flow","flight"]
+        Output: "fl"
 
     Example 2:
-
-    Input: strs = ["dog","racecar","car"]
-    Output: ""
-    Explanation: There is no common prefix among the input strings.
-
-    
+        Input: strs = ["dog","racecar","car"]
+        Output: ""
+        Explanation: There is no common prefix among the input strings.
 
     Constraints:
-
-    - 1 <= strs.length <= 200
-    - 0 <= strs[i].length <= 200
-    - strs[i] consists of only lowercase English letters if it is non-empty.
+        - 1 <= strs.length <= 200
+        - 0 <= strs[i].length <= 200
+        - strs[i] consists of only lowercase English letters if it is non-empty.
  */
 
 public class Solution {
