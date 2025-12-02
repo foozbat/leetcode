61fa4c52821c27d44c1dba4e49fefa097a310045
foozbat <github@foozbat.net>
2025-12-02 15:09:16 -0600

    Update 104

--- easy/104-maximum-depth-of-binary-tree.java
+++ easy/104-maximum-depth-of-binary-tree.java
@@ -8,16 +8,16 @@
  **/
 public class TreeNode {
     int val;
-     TreeNode left;
-     TreeNode right;
-     TreeNode() {}
-     TreeNode(int val) { this.val = val; }
-     TreeNode(int val, TreeNode left, TreeNode right) {
-         this.val = val;
-         this.left = left;
-         this.right = right;
-     }
- }
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 /**
  * Solution
