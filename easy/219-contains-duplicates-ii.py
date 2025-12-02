11d72da1c252516dd97c6b2515cd8a0b68e9ae8d
foozbat <github@foozbat.net>
2025-12-02 13:36:25 -0600

    Add 219

---
+++ easy/219-contains-duplicates-ii.py
@@ -0,0 +1,23 @@
+"""
+219. Contains Duplicate II
+https://leetcode.com/problems/contains-duplicate-ii/
+"""
+
+"""
+Solution
+Use a dictionary to store the last index of each number seen. For each number, check if it has been seen before check abs(i - j) <= k.
+"""
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        previous_nums = {}
+
+        for i in range(0, len(nums)):
+            if (nums[i] in previous_nums):
+                if abs(i - previous_nums[nums[i]]) <= k:
+                    return True
+                else:
+                    previous_nums[nums[i]] = i
+            else:
+                previous_nums[nums[i]] = i
+        
+        return False
