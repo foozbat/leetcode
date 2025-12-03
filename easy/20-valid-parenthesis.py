29c80391f7fe1ce945cc420ffc4c314959c25cf2
foozbat <github@foozbat.net>
2025-12-03 14:35:19 -0600

    Update 20

--- easy/20-valid-parenthesis.py
+++ easy/20-valid-parenthesis.py
@@ -1,39 +1,12 @@
 """
-    20. Valid Parentheses
-
-    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-    An input string is valid if:
-
-        Open brackets must be closed by the same type of brackets.
-        Open brackets must be closed in the correct order.
-        Every close bracket has a corresponding open bracket of the same type.
-
-    Example 1:
-        Input: s = "()"
-        Output: true
-
-    Example 2:
-        Input: s = "()[]{}"
-        Output: true
-
-    Example 3:
-        Input: s = "(]"
-        Output: false
-
-    Example 4:
-        Input: s = "([])"
-        Output: true
-
-    Example 5:
-        Input: s = "([)]"
-        Output: false
-
-    Constraints:
-        1 <= s.length <= 104
-        s consists of parentheses only '()[]{}'.
+20. Valid Parentheses
+https://leetcode.com/problems/valid-parentheses/
 """
 
+"""
+Solution
+Use a stack to track opening parentheses, ensuring each closing parenthesis matches the most recent opening one.
+"""
 class Solution:
     def isValid(self, s: str) -> bool:
         open_parens = ['(', '[', '{']
