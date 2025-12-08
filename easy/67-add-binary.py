e6e095af44d75996b6d6c4ba7d89a8fdf6292ea6
foozbat <github@foozbat.net>
2025-12-08 15:23:10 -0600

    Add 67

---
+++ easy/67-add-binary.py
@@ -0,0 +1,19 @@
+"""
+67. Add Binary
+https://leetcode.com/problems/add-binary/
+"""
+
+"""
+Solution
+Use python builtins to compute
+"""
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        total = int(a, 2) + int(b, 2)
+
+        return bin(total)[2:]
