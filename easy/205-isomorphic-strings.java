8ab0308ce2f6d8285eaf03d9841e752f954eac6e
foozbat <github@foozbat.net>
2025-12-11 21:58:13 -0600

    Update 205

--- easy/205-isomorphic-strings.java
+++ easy/205-isomorphic-strings.java
@@ -1,45 +1,16 @@
-/*
-    205. Isomorphic Strings
+/**
+ * 205. Isomorphic Strings
+ * https://leetcode.com/problems/isomorphic-strings/
+ */
 
-    Given two strings s and t, determine if they are isomorphic.
-
-    Two strings s and t are isomorphic if the characters in s can be replaced to get t.
-
-    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
-    Example 1:
-        Input: s = "egg", t = "add"
-        Output: true
-        Explanation:
-        The strings s and t can be made identical by:
-            Mapping 'e' to 'a'.
-            Mapping 'g' to 'd'.
-
-    Example 2:
-        Input: s = "foo", t = "bar"
-        Output: false
-        Explanation:
-        The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
-
-    Example 3:
-        Input: s = "paper", t = "title"
-        Output: true
-
-    Constraints:
-        1 <= s.length <= 5 * 10^4
-        t.length == s.length
-        s and t consist of any valid ascii character.
-*/
-
-/*
-    Solution:
-
-    Use two hash maps to store the character mappings from s to t and from t to s.
-    Iterate through the characters of both strings simultaneously.
-    Store a mapping of s to t and t to s, if the mapping already exists and it does not match the current character, return false.
-
-    Note: This java solution is fairly slow.
-*/
+/**
+ * Solution:
+ * Use two hash maps to store the character mappings from s to t and from t to s.
+ * Iterate through the characters of both strings simultaneously.
+ * Store a mapping of s to t and t to s, if the mapping already exists and it does not match the current character, return false.
+ *
+ * Note: This java solution is fairly slow.
+ */
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         HashMap<Character, Character> st = new HashMap<>();
