0039115f65d84f223c6973c9f30c60105706f807
foozbat <github@foozbat.net>
2025-12-11 22:04:25 -0600

    Update 1513

--- medium/1513-number-of-substrings-with-only-1s.c
+++ medium/1513-number-of-substrings-with-only-1s.c
@@ -1,31 +1,15 @@
-/*
-    1513. Number of Substrings With Only 1s
+/**
+ * 1513. Number of Substrings With Only 1s
+ * https://leetcode.com/problems/number-of-substrings-with-only-1s/
+ */
 
-    Given a binary string s, return the number of substrings with all characters 1's. Since the answer may be too large, return it modulo 109 + 7.
-
-    Example 1:
-        Input: s = "0110111"
-        Output: 9
-        Explanation: There are 9 substring in total with only 1's characters.
-        "1" -> 5 times.
-        "11" -> 3 times.
-        "111" -> 1 time.
-
-    Example 2:
-        Input: s = "101"
-        Output: 2
-        Explanation: Substring "1" is shown 2 times in s.
-
-    Example 3:
-        Input: s = "111111"
-        Output: 21
-        Explanation: Each substring contains only 1's characters.
-
-    Constraints:
-        1 <= s.length <= 105
-        s[i] is either '0' or '1'.
-*/
-
+/**
+ * Solution
+ * Iterate through the string, counting consecutive '1's. When a '0' is encountered
+ * or the end of the string is reached, use the formula k*(k+1)/2 to calculate
+ * the number of substrings that can be formed from the consecutive '1's counted so far.
+ * Sum these counts and return the total modulo 10^9 + 7.
+ */
 int numSub(char* s) {
     int i = 0;
     long current_count = 0;
