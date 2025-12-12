1d17085655999c71e2d5958d7683f28e226ab754
foozbat <github@foozbat.net>
2025-12-11 22:02:53 -0600

    Update 392

--- easy/392-is-subsequence.c
+++ easy/392-is-subsequence.c
@@ -1,28 +1,13 @@
 /*
-    Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+ * 392. Is Subsequence
+ * https://leetcode.com/problems/is-subsequence/
+ */
 
-    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
-
-    Example 1:
-        Input: s = "abc", t = "ahbgdc"
-        Output: true
-
-    Example 2:
-        Input: s = "axc", t = "ahbgdc"
-        Output: false
-
-    Constraints:
-        0 <= s.length <= 100
-        0 <= t.length <= 104
-        s and t consist only of lowercase English letters.
-*/
-
-/*
-    Solution:
-
-    Two pointers approach. Iterate through both strings. Increment pointer for s only when there is a match.
-    Return true if we reach the end of s.
-*/
+/**
+ * Solution
+ * Two pointers approach. Iterate through both strings. Increment pointer for s only when there is a match.
+ * Return true if we reach the end of s.
+ */
 bool isSubsequence(char* s, char* t) {
     int i = 0;
     int j = 0;
